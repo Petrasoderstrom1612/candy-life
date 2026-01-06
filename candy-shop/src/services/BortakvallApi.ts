7f80b0235a5b47b3370e379372b90dd6e442ddfe
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-06 11:29:47 +0100

    set up axios - both imports and declarations and imported BASE_URL that is now string thanks to vite-en.d.ts

---
+++ candy-shop/src/services/BortakvallApi.ts
@@ -0,0 +1,7 @@
+// import axios from "axios";
+// import { AxiosError } from 'axios';
+// import type {Tag, Candy, CandyWithDescription} from "./Types"
+
+console.log(import.meta.env.VITE_API_BASEURL)
+const BASE_URL = import.meta.env.VITE_API_BASEURL
+console.log(BASE_URL)
