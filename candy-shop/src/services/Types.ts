82ced353a1fc856cbc7df8ba8c1ca6ef82325136
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-09 15:43:36 +0100

    added Order and Order item types

--- candy-shop/src/services/Types.ts
+++ candy-shop/src/services/Types.ts
@@ -38,6 +38,24 @@ export type CartContextType = {
   toggleCart: () => void;
 };
 
+interface OrderItem {
+  product_id: number;
+  qty: number;
+  item_price: number;
+  item_total: number;
+}
+
+export type Order = {
+  customer_first_name: string;
+  customer_last_name: string;
+  customer_address: string;
+  customer_postcode: string;
+  customer_city: string;
+  customer_email: string;
+  customer_phone?: string;
+  order_total: number;
+  order_items: OrderItem[];
+}
 
 export interface Tag {
   id: number;
