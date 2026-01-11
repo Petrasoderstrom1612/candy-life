3cfe5bc5f4f06f0456f19fbc15cae14c82b2293c
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-11 14:57:43 +0100

    created a form

--- candy-shop/src/components/Checkout.tsx
+++ candy-shop/src/components/Checkout.tsx
@@ -1,12 +1,27 @@
-// import React from 'react'
 
-// const Checkout = () => {
-//   return (
-//     <form>
-      
-//       <button></button>
-//     </form>
-//   )
-// }
+const Checkout = () => {
+  return (
+    <form>
+      <label htmlFor="email">First name:</label>
+      <input id="firstname" type="text" name="customer_first_name" required></input>
+      <br />
+      <label htmlFor="email">Last name:</label>
+      <input id="lastname" type="text" name="customer_last_name" required></input>
+      <br />
+      <label htmlFor="postcode">Postcode:</label>
+      <input id="postcode" type="number" name="customer_postcode" required></input>
+      <br />
+      <label htmlFor="city">City:</label>
+      <input id="city" type="text" name="customer_city" required></input>
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input id="email" type="email" name="customer_email" required></input>
+      <br />
+      <label htmlFor="phone">Mobile phone:</label>
+      <input id="phone" type="number" name="customer_phone"></input>
+      <button>Beställ</button>
+    </form>
+  )
+}
 
-// export default Checkout
+export default Checkout
