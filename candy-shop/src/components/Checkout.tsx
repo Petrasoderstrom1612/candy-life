8cb85f75bff3c68a09e4c702464464c23d6dde54
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-01-11 16:26:10 +0100

    max characters

--- candy-shop/src/components/Checkout.tsx
+++ candy-shop/src/components/Checkout.tsx
@@ -19,31 +19,36 @@ return (
         <fieldset>
           <div className="form-group">
             <label htmlFor="firstname">Förnamn</label>
-            <input id="firstname" type="text" name="customer_first_name" required />
+            <input id="firstname" type="text" name="customer_first_name" required maxLength={255}/>
           </div>
 
           <div className="form-group">
             <label htmlFor="lastname">Efternamn</label>
-            <input id="lastname" type="text" name="customer_last_name" required />
+            <input id="lastname" type="text" name="customer_last_name" required maxLength={255}/>
           </div>
         </fieldset>
 
         <fieldset>
           <div className="form-group">
+            <label htmlFor="address">Adress</label>
+            <input id="address" type="text" name="customer_address" required maxLength={255}/>
+          </div>
+
+          <div className="form-group">
             <label htmlFor="postcode">Postnummer</label>
-            <input id="postcode" type="number" name="customer_postcode" required />
+            <input id="postcode" type="text" name="customer_postcode" required maxLength={6} pattern="\d{2,6}" title="Postnummer får vara max 6 siffror"/>
           </div>
 
           <div className="form-group">
             <label htmlFor="city">Stad</label>
-            <input id="city" type="text" name="customer_city" required />
+            <input id="city" type="text" name="customer_city" required maxLength={255} />
           </div>
         </fieldset>
 
         <fieldset>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input id="email" type="email" name="customer_email" required />
+            <input id="email" type="email" name="customer_email" required maxLength={255}/>
           </div>
 
           <div className="form-group">
